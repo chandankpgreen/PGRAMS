981e9594d7e2769d9081955712367f66e9724a0f
chandankpgreen <chandankpgreen@gmail.com>
2016-01-30 14:13:53 +0530

    Initial commit for PGRAMS

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PGRAMS_CS.Startup))]
+namespace PGRAMS_CS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
